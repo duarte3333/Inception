23439ee8e08eb1d95fdc060a3f9847e3b96ba5dc
duarte_3333 <dsmorais.33@gmail.com>
2024-08-10 20:10:25 +0100

    added final version

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,14 +1,14 @@
+
+# Modify PHP-FPM configuration
 sed -ie "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/" /etc/php/7.4/fpm/pool.d/www.conf
-#sed is used for replacing the string in the file with the new string 
 
-#everything that has the container name it replaces by the ip address
-
+# Print environment variables
 echo "---------------------------------"
 wall " Printing the environment variables"
 echo "MYSQL_DATABASE: $MYSQL_DATABASE"
 echo "MYSQL_USER: $MYSQL_USER"
 echo "MYSQL_PASSWORD: $MYSQL_PASSWORD"
-echo "HOST_NAME: $HOST_NAME"
+echo "DB_HOST: $HOST_NAME"
 echo "WP_URL: $WP_URL"
 echo "WP_TITLE: $WP_TITLE"
 echo "WP_ADMIN: $WP_ADMIN"
@@ -17,31 +17,44 @@ echo "WP_USER: $WP_USER"
 echo "WP_USER_PASSWORD: $WP_USER_PASSWORD"
 echo "---------------------------------"
 
+# Check if wp-config.php exists
+if [ ! -f /var/www/html/wp-config.php ]; then
+    mv ./wp-config.php /var/www/html/
+    cd /var/www/html/
+    ls
 
-if [ ! -f /var/www/html/wp-config.php ]; then #check if the file exists
-	mv ./wp-config.php /var/www/html/
-	cd /var/www/html/ #change the directory to /var/www/html
-	ls
+    # Change the ownership of the wp-config.php file to the current user
+    chown $(whoami):$(whoami) /var/www/html/wp-config.php
 
-	chown $(whoami):$(whoami) /var/www/html/wp-config.php
-
-    #this command is to replace the string in the file wp-config.php with the new string
+    # Replace placeholders in wp-config.php with actual values
     sed -i "s/__MYSQL_DATABASE__/'$MYSQL_DATABASE'/g" /var/www/html/wp-config.php 
     sed -i "s/__MYSQL_USER__/'$MYSQL_USER'/g" /var/www/html/wp-config.php
     sed -i "s/__MYSQL_PASSWORD__/'$MYSQL_PASSWORD'/g" /var/www/html/wp-config.php
     sed -i "s/__DOCKERCONTAINERNAME__/'$HOST_NAME'/g" /var/www/html/wp-config.php
 
-	wp core download --allow-root
-	until mysqladmin -hmariadb -u${MYSQL_USER} -p${MYSQL_PASSWORD} ping; do
+    # Download WordPress core
+    wp core download --allow-root
+
+    # Wait for the database to be ready
+    until mysqladmin -hmariadb -u${MYSQL_USER} -p${MYSQL_PASSWORD} ping; do
        sleep 2
     done
-	wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb:3306 --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-	wp core install --url=${WP_URL} --title="${WP_TITLE}" --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email="$WP_ADMIN@student.42.fr" --allow-root
-	wp user create ${WP_USER} "$WP_USER"@user.com --role=author --user_pass=${WP_USER_PASSWORD} --allow-root
 
-	chown -R www-data:www-data /var/www/html/wp-content #change the owner of the directory to www-data
+    # Create WordPress configuration
+    # wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb:3306 --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+
+    # Install WordPress
+    wp core install --url=${WP_URL} --title="${WP_TITLE}" --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email="$WP_ADMIN@student.42.fr" --allow-root
+
+    # Create a WordPress user
+    wp user create ${WP_USER} "$WP_USER"@user.com --role=author --user_pass=${WP_USER_PASSWORD} --allow-root
+
+    # Change the owner of the wp-content directory to www-data
+    chown -R www-data:www-data /var/www/html/wp-content
 fi
 
+# Display the wp-config.php file
 cat /var/www/html/wp-config.php | grep "define( ')"
 
-exec /usr/sbin/php-fpm7.4 -F #start the php-fpm service and keep it running in the foreground 
+# Start PHP-FPM in the foreground
+exec /usr/sbin/php-fpm7.4 -F
